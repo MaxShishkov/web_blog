70f76792d0b52e7a0b4bb15b06c0e672fd986c46
Max Shishkov <maxshishkov92@gmail.com>
2026-02-12 18:28:09 -0600

    add env

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+from os import path, environ
+from dotenv import load_dotenv
 from flask_login import LoginManager
 from .views import views
 from .auth import auth
 
 def create_app():
+    load_dotenv()
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = ""
+    app.config['SECRET_KEY'] = environ.get("SECRET_KEY")
     app.register_blueprint(views)
     app.register_blueprint(auth)
     
