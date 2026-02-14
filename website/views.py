5e91f052c4b50aac8eff79a16dcf2a03a92b452c
Max Shishkov <maxshishkov92@gmail.com>
2026-02-14 15:51:53 -0600

    feat: add create post get request

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__, url_prefix="/")
@@ -8,3 +8,11 @@ views = Blueprint("views", __name__, url_prefix="/")
 @login_required
 def home():
     return render_template("home.html", name=current_user.username)
+
+@views.route("/create-post", methods=["GET", "POST"])
+@login_required
+def create_post():
+    if request.method == "POST":
+        pass
+    
+    return render_template("create_post.html")
